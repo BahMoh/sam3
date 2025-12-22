40da0044e0baa79b038a21518e05a2a4bb227ef9
Mohamad Alikhani <87755081+BahMoh@users.noreply.github.com>
2025-12-22 22:54:54 +0330

    return back to normal

--- sam3/model_builder.py
+++ sam3/model_builder.py
@@ -648,12 +648,12 @@ def build_sam3_image_model(
 
     if load_from_HF and checkpoint_path is None:
         checkpoint_path = download_ckpt_from_hf()
-        print("Loading model from condition if load_from_HF and checkpoint_path is None!")
+        # print("Loading model from condition if load_from_HF and checkpoint_path is None!")
     # Load checkpoint if provided
     if checkpoint_path is not None:
-        print(f"Loading model from: {checkpoint_path}")
+        # print(f"Loading model from: {checkpoint_path}")
         _load_checkpoint(model, checkpoint_path)
-        print("Loading model from condition if checkpoint_path is not None!, checkpoint_path")
+        # print("Loading model from condition if checkpoint_path is not None!, checkpoint_path")
 
     # Setup device and mode
     model = _setup_device_and_mode(model, device, eval_mode)
